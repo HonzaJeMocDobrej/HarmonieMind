45baead7a449d6b0f201f02056b854e89618e6fd
Honzak <jfedor06@gmail.com>
2023-07-25 12:01:32 +0200

    SEO and changes - done

--- res/js/index.js
+++ res/js/index.js
@@ -6,6 +6,7 @@ let menu = document.getElementById('menu')
 let header = document.getElementById('headerItemsId')
 let hoverPhone = document.getElementById('hover-phone')
 let hoverMail = document.getElementById('hover-mail')
+let facebook = document.getElementById('facebook')
 let children = header.children
 
 let isOpened = false
@@ -33,12 +34,31 @@ const focus = (text, item, path, img) => {
 focus(phone, content1, "tel:704721304", hoverPhone)
 focus(mail, content2, "mailto:konzultace@harmoniemind.cz", hoverMail)
 
+facebook.onmouseover = () => {
+    facebook.style.cursor = 'pointer'
+    facebook.style.transform = 'scale(0.95)'
+}
+
+facebook.onmouseout = () => {
+    facebook.style.cursor = 'pointer'
+    facebook.style.transform = 'scale(1)'
+}
+
+facebook.onclick = () => {
+    window.open('https://www.facebook.com/profile.php?id=100094818530958', '_blank') 
+
+}
+
 menu.onmouseover = () => {
     menu.style.cursor = "pointer"
 }
 
 menu.onclick = () => {
     console.log("clicked")
+    isOpened ? facebook.style.display = 'none' : facebook.style.display = 'block'
+    if (!isOpened && window.innerWidth < 1200) {
+        facebook.style.margin = '0 auto'
+    }
     for (let i = 0; i < children.length; i++) {
         if (children[i].tagName === 'A') {
             isOpened ?
@@ -46,10 +66,15 @@ menu.onclick = () => {
             children[i].style.display = "block"
         }
     }
+    header.appendChild
     isOpened = !isOpened
 }
 
 window.onresize = () => {
+    if (!isOpened && window.innerWidth > 1200) {
+        facebook.style.margin = '0'
+    }
+
     if (window.innerWidth > 1200) {
         for (let i = 0; i < children.length; i++) {
             children[i].style.display = 'block' 
--- res/js/template.js
+++ res/js/template.js
@@ -1,8 +1,25 @@
 let menu = document.getElementById('menu')
 let header = document.getElementById('headerItemsId')
+let facebook = document.getElementById('facebook')
 let children = header.children
 
 let isOpened = false
+
+facebook.onmouseover = () => {
+    facebook.style.cursor = 'pointer'
+    facebook.style.transform = 'scale(0.95)'
+}
+
+facebook.onmouseout = () => {
+    facebook.style.cursor = 'pointer'
+    facebook.style.transform = 'scale(1)'
+}
+
+facebook.onclick = () => {
+    window.open('https://www.facebook.com/profile.php?id=100094818530958', '_blank') 
+
+}
+
 
 menu.onmouseover = () => {
     menu.style.cursor = "pointer"
@@ -10,6 +27,10 @@ menu.onmouseover = () => {
 
 menu.onclick = () => {
     console.log("clicked")
+    isOpened ? facebook.style.display = 'none' : facebook.style.display = 'block'
+    if (!isOpened && window.innerWidth < 1200) {
+        facebook.style.margin = '0 auto'
+    }
     for (let i = 0; i < children.length; i++) {
         if (children[i].tagName === 'A') {
             isOpened ?
@@ -17,10 +38,15 @@ menu.onclick = () => {
             children[i].style.display = "block"
         }
     }
+    header.appendChild
     isOpened = !isOpened
 }
 
 window.onresize = () => {
+    if (!isOpened && window.innerWidth > 1200) {
+        facebook.style.margin = '0'
+    }
+
     if (window.innerWidth > 1200) {
         for (let i = 0; i < children.length; i++) {
             children[i].style.display = 'block' 
